310d9f08982087ba4e067b178ffee4f84c04ca39
dag1sss375-hash <dag1sss375@gmail.com>
2026-04-28 20:07:05 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const tg = window.Telegram.WebApp;
-tg.expand();
-
-// Hozircha oddiy test
-console.log("SkinOz ishga tushdi!");
-
-function openCase(caseId) {
-    tg.sendData('open_case:' + caseId);
-}
